b9e096111e8b0e8d5735f68f9aec93715f10ad79
Javivy <84439052+Javivy@users.noreply.github.com>
2022-11-01 15:59:08 -0400

    Create react app

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/ToDoContainer.js';
+
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
